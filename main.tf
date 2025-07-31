3d751a4d0032ef57f8e8e5fa98c80c52781872cc
Akash Kumar <akash.kumar@goapl.com>
2025-07-31 14:37:41 +0530

    creating new infra

--- main.tf
+++ main.tf
@@ -20,3 +20,13 @@ module "compute" {
   network      = module.vpc.network_self_link
   subnetwork   = module.vpc.subnetwork_self_link
 }
+
+module "compute" {
+  source       = "git::https://github.com/sourrexa/terraform-gcp-modules.git//compute?ref=v1.0.0"
+  name         = "vm-2"
+  machine_type = "e2-medium"
+  zone         = "us-central1-b"
+  image        = "debian-cloud/debian-12"
+  network      = module.vpc.network_self_link
+  subnetwork   = module.vpc.subnetwork_self_link
+}
