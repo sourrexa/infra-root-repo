f66b997a0aafc1dfc9aea71ecb133beceaa31c02
Akash Kumar <akash.kumar@goapl.com>
2025-07-31 13:57:21 +0530

    creating new infra

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 module "vpc" {
-  source                  = "git::https://github.com/sourrexa/terraform-gcp-modules.git//vpc?ref=v1.0.1"
+  source                  = "git::https://github.com/sourrexa/terraform-gcp-modules.git//vpc?ref=v1.0.0"
   name                    = "my-vpc"
   auto_create_subnetworks = false
 }
 
 module "bucket" {
-  source   = "git::https://github.com/sourrexa/terraform-gcp-modules.git//bucket?ref=v1.0.1"
+  source   = "git::https://github.com/sourrexa/terraform-gcp-modules.git//bucket?ref=v1.0.0"
   name     = "my-demo-bucket-5678"
   location = "us-central1"
 }
 
 module "compute" {
-  source       = "git::https://github.com/sourrexa/terraform-gcp-modules.git//compute?ref=v1.0.1"
+  source       = "git::https://github.com/sourrexa/terraform-gcp-modules.git//compute?ref=v1.0.0"
   name         = "vm-1"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
